303851544a99ad0c4ea50e0299efb8c137b3b867
pizzadudez <40966394+pizzadudez@users.noreply.github.com>
2019-09-17 20:07:32 +0300

    gitignore

--- backend/src/models/Auth.js
+++ backend/src/models/Auth.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./src/db.sqlite3', err => {
+const db = new sqlite3.Database('./db.sqlite3', err => {
   if (err) {
     console.log(err);
     return;
